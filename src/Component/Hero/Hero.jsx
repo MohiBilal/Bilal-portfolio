1235d58e55c4dae11671b5c5cb6b6a69a053d058
Bilal Zia <93026115+MohiBilal@users.noreply.github.com>
2024-06-13 09:09:57 -0700

    added styles in hero component

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import styles from "./Hero.module.css"
 
 export default function Hero() {
   return (
-    <section>
-        <div>
-            <h1>Hi, </h1>
-            <p>jdnkandkadnkadnk</p>
-            <a href="mailto:myemail@email.com">Contact Me</a>
+    <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hi, </h1>
+            <p className={styles.description}>jdnkandkadnkadnk</p>
+            <a className={styles.contactBtn} href="mailto:myemail@email.com">Contact Me</a>
         </div>
 
-            <img src="https://raw.githubusercontent.com/CodeCompleteYT/react-portfolio/main/assets/hero/heroImage.png" alt="Avatar" />
+            <img className={styles.heroImg} src="https://raw.githubusercontent.com/CodeCompleteYT/react-portfolio/main/assets/hero/heroImage.png" alt="Avatar" />
+
+
+          <div className={styles.topBlur}/>
+          <div className={styles.bottomBlur}/>
 
     </section>
   )
