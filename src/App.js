69e1bac332708f4ce9c5d7429267ae366794a530
Bilal Zia <93026115+MohiBilal@users.noreply.github.com>
2024-06-13 01:18:45 -0700

    navbar added styling and menuIcon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Component/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Navbar/>
     </div>
   );
 }
---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import React, { useState }  from 'react'
+import './Navbar.css'
+// import menuIcon from "https://raw.githubusercontent.com/CodeCompleteYT/react-portfolio/main/assets/nav/menuIcon.png";
+
+export default function Navbar() {
+  return (
+    <nav className='navbarStyling'>
+        <a className='title' href="/">Portfolio</a>
+
+
+
+        <div className='menu'>
+
+            <img src="https://raw.githubusercontent.com/CodeCompleteYT/react-portfolio/main/assets/nav/menuIcon.png" alt="" /> {/*temporary img*/}
+
+            <ul className='menuItems'>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+
+                <li>
+                    <a href="#experience">Experience</a>
+                </li>
+
+                <li>
+                    <a href="#projects">Projects</a>
+                </li>
+
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+
+
+    </nav>
+  )
+}
