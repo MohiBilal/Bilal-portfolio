f6839468aa27b7a23ceff986e0820eb3d812affd
Bilal Zia <93026115+MohiBilal@users.noreply.github.com>
2024-06-14 06:23:31 -0700

    finished with about section

--- src/Component/About/About.jsx
+++ src/Component/About/About.jsx
@@ -4,7 +4,7 @@ import styles from './About.module.css'
 
 export default function About() {
   return (
-    <section className={styles.container}>
+    <section className={styles.container} id='about'>
         <h2 className={styles.title}>About</h2>
         <div className={styles.content}>
             {/* <img src={getImageUrl("about/aboutImage.png")} alt="icon" /> */}
@@ -14,7 +14,7 @@ export default function About() {
                 <li className={styles.aboutItem}>
                     <img src="https://raw.githubusercontent.com/CodeCompleteYT/react-portfolio/main/assets/about/cursorIcon.png" alt="Cursor Icon" />
                     <div className={styles.aboutItemText}>
-                        <h1>Frontend developer</h1>
+                        <h3>Frontend developer</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, corporis. Dolorum, nulla tempora repudiandae corrupti tenetur illo laudantium ratione, veniam distinctio corporis a obcaecati quae! Repellendus consequuntur consequatur ad ipsum.</p>
                     </div>    
                 </li>
@@ -22,7 +22,7 @@ export default function About() {
                 <li className={styles.aboutItem}>
                     <img src="https://raw.githubusercontent.com/CodeCompleteYT/react-portfolio/main/assets/about/serverIcon.png" alt="Server Icon" />
                     <div className={styles.aboutItemText}>
-                        <h1>Backend developer</h1>
+                        <h3>Backend developer</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ducimus veritatis quaerat odio natus blanditiis illum nobis quam eos praesentium! Molestias, iste rem? Assumenda esse quas nulla tenetur odio aliquam.</p>
                     </div>    
                 </li>
@@ -30,7 +30,7 @@ export default function About() {
                 <li className={styles.aboutItem}>
                     <img src="https://raw.githubusercontent.com/CodeCompleteYT/react-portfolio/main/assets/about/serverIcon.png" alt="UI Icon" />
                     <div className={styles.aboutItemText}>
-                        <h1>UI designer</h1>
+                        <h3>UI designer</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptate praesentium soluta. Officia quia officiis eveniet deleniti cumque quod nulla facere quae dolorum possimus praesentium quo, velit accusantium aliquam sunt.</p>
                     </div>    
                 </li>
